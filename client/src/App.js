36f5f9f98c6193b4a37f17503eac61a47b98cdcf
cchiranjib <cchiranjib0908@gmail.com>
2021-06-07 13:44:39 +0530

    Add Room file added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/screens/Home"
 import Profile from "./components/screens/Profile"
 import SignIn from "./components/screens/Signin"
 import SignUp from "./components/screens/Signup"
+import AddRoom from "./components/screens/AddRoom"
 //import CreatePost from "./components/screens/createPost"
 import {reducer,initialState} from "./reducer/userReducers"
 //import EditProfile from "./components/screens/editProfile"
@@ -44,6 +45,9 @@ const Routing = () => {
          </Route>
          <Route path="/profile">
             <Profile />
+         </Route>
+         <Route path="/add">
+            <AddRoom />
          </Route>
          {/* <Route path="/create">
             <CreatePost />
