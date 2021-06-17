60b0eb9b14ef3daa8f992e83273dd9d6ae4873c8
Himanshu Banate <banatehimanshu@gmail.com>
2021-06-17 12:02:49 +0530

    signup-signin-design

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,71 +1,103 @@
-import React,{useState,useContext} from "react"
-import {Link,useHistory} from "react-router-dom"
-import {UserContext} from "../../App"
-import M from "materialize-css"
-import "../../App.css"
+import React, { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "../../App";
+import M from "materialize-css";
+import "../../App.css";
 
 const SignIn = () => {
-    const {state,dispatch} = useContext(UserContext)
-    const history = useHistory()
-    const [user_id,setUserId] = useState("")
-    const [password,setPassword] = useState("")
-    const postData = () => {
-        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(user_id) & !/^(\+91-|\+91|0)?\d{10}$/.test(user_id)){
-           return M.toast({html: "Invalid UserId",classes: "#d32f2f red darken-2"})
-        }
-        fetch("/login",{       //used proxy to interact with http://localhost:5000
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                user_id,
-                password
-            })
-        })
-            .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    if(data.error){
-                        M.toast({html: data.error , classes:"#d32f2f red darken-2"})
-                    }
-                    else{
-                        localStorage.setItem("jwt",data.token)
-                        localStorage.setItem("user",JSON.stringify(data.user))
-                        dispatch({type:"USER",payload:data.user})
-                        M.toast({html: "signed in successfully" , classes:"#43a047 green darken-1"})
-                        history.push("/profile")
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                })
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  const [user_id, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const postData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        user_id
+      ) & !/^(\+91-|\+91|0)?\d{10}$/.test(user_id)
+    ) {
+      return M.toast({
+        html: "Invalid UserId",
+        classes: "#d32f2f red darken-2",
+      });
     }
-    return (
-        <div className="myCard">
-            <div className="card auth-card input-field">
+    fetch("/login", {
+      //used proxy to interact with http://localhost:5000
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          M.toast({ html: data.error, classes: "#d32f2f red darken-2" });
+        } else {
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
+          M.toast({
+            html: "signed in successfully",
+            classes: "#43a047 green darken-1",
+          });
+          history.push("/profile");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="container home-card">
+      <div className="section">
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="home-image">
+              <img
+                className="responsive-img"
+                src="https://cdn.pixabay.com/photo/2017/01/23/16/35/home-2003067_960_720.png"
+                alt="Rooms"
+              />
+            </div>
+          </div>
+          <div className="col s12 m6">
+            <div className="myCard">
+              <div className="card auth-card input-field">
                 <h2 className="brand-logo">ROOMWALA</h2>
                 <input
-                    type="text"
-                    placeholder="Email/Phone"
-                    value={user_id}
-                    onChange={(e) => setUserId(e.target.value)}
+                  type="text"
+                  placeholder="Email/Phone"
+                  value={user_id}
+                  onChange={(e) => setUserId(e.target.value)}
                 />
                 <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="waves-effect waves-light btn red accent-4 pulse" onClick={()=>postData()}>
-                    Login
+                <button
+                  className="waves-effect waves-light btn red accent-4 pulse"
+                  onClick={() => postData()}
+                >
+                  Login
                 </button>
                 <h6>
-                    <Link className="link" to="/signup">Don't have an account</Link>
+                  <Link className="link" to="/signup">
+                    Don't have an account
+                  </Link>
                 </h6>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,83 +1,110 @@
-import React,{useState} from "react"
-import {Link,useHistory} from "react-router-dom"
-import M from "materialize-css"
-import "../../App.css"
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
+import "../../App.css";
 
 const SignUp = () => {
-    const history = useHistory()
-    const [name,setName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [phone,setPhone] = useState("")
-    const postData = () => {
-        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-           return M.toast({html: "Invalid Email",classes: "#d32f2f red darken-2"})
-        }
-        fetch("/signup",{       //used proxy to interact with http://localhost:5000
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                name,
-                email,
-                phone,
-                password
-            })
-        })
-            .then(res => res.json())
-                .then(data => {
-                    if(data.error){
-                        M.toast({html: data.error , classes:"#d32f2f red darken-2"})
-                    }
-                    else{
-                        M.toast({html: data.message , classes:"#43a047 green darken-1"})
-                        history.push("/login")
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                })
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const postData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      return M.toast({
+        html: "Invalid Email",
+        classes: "#d32f2f red darken-2",
+      });
     }
-    return (
-
-        
-        <div className="myCard">
-            <div className="card auth-card input-field">
+    fetch("/signup", {
+      //used proxy to interact with http://localhost:5000
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({ html: data.error, classes: "#d32f2f red darken-2" });
+        } else {
+          M.toast({ html: data.message, classes: "#43a047 green darken-1" });
+          history.push("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="container home-card">
+      <div className="section">
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="home-image">
+              <img
+                className="responsive-img"
+                src="https://cdn.pixabay.com/photo/2017/01/23/16/35/home-2003067_960_720.png"
+                alt="Rooms"
+              />
+            </div>
+          </div>
+          <div className="col s12 m6">
+            <div className="myCard">
+              <div className="card auth-card input-field">
                 <h2 className="brand-logo">ROOMWALA</h2>
                 <input
-                    type="text"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
                 <input
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
-                    type="tel"
-                    placeholder="Phone Number"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
+                  type="tel"
+                  placeholder="Phone Number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
                 />
                 <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="waves-effect waves-light btn red accent-4 pulse" onClick={()=>postData()}>
-                    SignUp
+                <button
+                  className="waves-effect waves-light btn red accent-4 pulse"
+                  onClick={() => postData()}
+                >
+                  SignUp
                 </button>
                 <h6>
-                    <Link className="link" to="/login">Already have an account</Link>
+                  <Link className="link" to="/login">
+                    Already have an account
+                  </Link>
                 </h6>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
